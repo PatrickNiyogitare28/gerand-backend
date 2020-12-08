b3ca419b81e3526b4d891d1eed8b3e890e2bd830
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2020-12-08 12:37:46 +0200

    fit: added update project fit

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, NotAcceptableException, NotFoundException } from '@nestjs/common';
+import { Injectable, NotAcceptableException, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import {InjectModel} from '@nestjs/mongoose';
 import {Model} from 'mongoose';
 import { Project } from './project.model';
 import {AuthService} from '../auth/auth.service';
 import {User} from '../users/user.model';
 import {UserType} from '../utils/enums/userType';
+import { update } from 'lodash';
 
 @Injectable()
 export class ProjectsService {
@@ -47,10 +48,9 @@ export class ProjectsService {
        const currentUser: any = await this.authService.decodeToken(req);
        let project;
        try{
-           if(currentUser.userType != UserType.admin){
+           if(currentUser.userType != UserType.admin)
             return await this.ProjectModel.findOne({_id: projectId, users: currentUser._id});
-           }
-
+           
            else
             return  await this.ProjectModel.findOne({_id: projectId});
        }
@@ -58,7 +58,46 @@ export class ProjectsService {
            throw new NotFoundException("Project with Id "+projectId+" not found");
        }
     }
+     
+   async updateProject(projectId: string,data:any, req:any){
+       const currentUser:any = await this.authService.decodeToken(req);
+       const owner = currentUser._id;
+       let updatedProject;
+       try{
+        updatedProject =  await this.ProjectModel.findById(projectId);
+        if(!updatedProject)
+        throw new NotFoundException("Project not found");
+       }
+       catch(e){
+           throw new NotFoundException("Project not found");
+       }
+       const isProjectOwner = await this.ProjectModel.findOne({owner: currentUser._id});
+       if(!isProjectOwner && !currentUser.UserType.admin)
+       throw new UnauthorizedException('No access to update the project');
+
+       const {projectName,projectType,users} = data;
        
+       if(users && users.length > 0)
+       {
+        for(let i = 0; i < users.length; i++){
+            await this.findDuplicates(users, users[i]);
+           } 
+          users.push(owner);
+       }
+      
+
+       if(projectName)
+       updatedProject.projectName = projectName;
+       
+       if(projectType)
+       updatedProject.projectType = projectType;
+
+       if(users)
+       updatedProject.users = users;
+
+       return await updatedProject.save();
+
+   }
     async findUserById(id: string){
         try{
             const user =await this.usersModel.findOne({_id: id, accountStatus: 1});   
